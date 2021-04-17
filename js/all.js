d79ce9dba395ddc9a1b5e334353a7a9c102a32d0
Dyu <48444399+yuuuu0311@users.noreply.github.com>
2021-04-18 01:07:39 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,26 @@
+console.log('import success');
+
+let tag = document.querySelector(".tag");
+let comeBack = new Date("Apr 25, 2021 00:00:00").getTime();
+
+// console.log(comeBack);
+
+setInterval(
+    function() {
+        let now = new Date().getTime();
+        let remain = comeBack - now;
+    
+        let days = Math.floor(remain/(1000 *60 *60 *24))
+        let hours = Math.floor((remain % (1000 *60* 60 *24)) / (1000* 60 *60))
+        let mins = Math.floor((remain % (1000 *60 *60)) / (1000 *60))
+        let seconds = Math.floor((remain % (1000 *60)) / 1000)
+
+        tag.innerHTML = `${days}<span>day</span> ${hours}<span>hour</span> ${mins}<span>min</span> ${seconds}<span>sec</span>`
+
+        if(remain <= 0){
+            tag.innerHTML = `Welcome Back`
+        }
+    },1000
+
+
+)
