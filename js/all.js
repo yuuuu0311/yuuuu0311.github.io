535ba8f801d4893832f99026afb1b534e34310ee
Dyu <48444399+yuuuu0311@users.noreply.github.com>
2021-04-18 15:47:48 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,14 +1,17 @@
 console.log('import success');
 
 let tag = document.querySelector(".tag");
-let comeBack = new Date("Mar 11, 2021 00:00:00").getTime();
+let text = document.querySelector('.text')
+
+let comeBack = new Date("Apr 25, 2021 00:00:00").getTime();
+let quarantine = new Date("May 11, 2021 00:00:00").getTime();
+
 
 // console.log(comeBack);
-
-setInterval(
+let x = setInterval(
     function() {
         let now = new Date().getTime();
-        let remain = comeBack - now;
+        let remain = comeBack - now
     
         let days = Math.floor(remain/(1000 *60 *60 *24))
         let hours = Math.floor((remain % (1000 *60* 60 *24)) / (1000* 60 *60))
@@ -16,11 +19,36 @@ setInterval(
         let seconds = Math.floor((remain % (1000 *60)) / 1000)
 
         tag.innerHTML = `${days}<span>day</span> ${hours}<span>hour</span> ${mins}<span>min</span> ${seconds}<span>sec</span>`
-
+        text.innerHTML = `till Karna back`
         if(remain <= 0){
-            tag.innerHTML = `Welcome Back`
+            clearInterval(x)
+            quarantineTime()
         }
     },1000
-
-
 )
+
+function quarantineTime() {
+
+    let y = setInterval(
+        function() {
+            let now = new Date().getTime();
+            let remain = quarantine - now
+
+            let days = Math.floor(remain/(1000 *60 *60 *24))
+            let hours = Math.floor((remain % (1000 *60* 60 *24)) / (1000* 60 *60))
+            let mins = Math.floor((remain % (1000 *60 *60)) / (1000 *60))
+            let seconds = Math.floor((remain % (1000 *60)) / 1000)
+
+            tag.innerHTML = `${days}<span>day</span> ${hours}<span>hour</span> ${mins}<span>min</span> ${seconds}<span>sec</span>`
+            text.innerHTML = `till quarantine over`
+
+            if(remain <= 0){
+                tag.innerHTML = `Karna is back`
+                text.innerHTML = ``
+                clearInterval(y)
+            }
+        },1000
+    )
+
+}
+
