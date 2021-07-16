152d41f37bedfb8d58b0c03ddf2a2f1fdc95c951
Dyu <48444399+yuuuu0311@users.noreply.github.com>
2021-07-17 00:06:06 +0800

    foodpanda mockup
    version 2

--- foodpanda/js/restaurant.js
+++ foodpanda/js/restaurant.js
@@ -1,35 +1,23 @@
-console.log('restaurant');
+const loCation = ['台北','新北','基隆','新竹','宜蘭','桃園','台南','高雄']
+
+let cityContainer = document.querySelector('.city')
+let cityCard = []
+let content = ''
+
+for (i = 0; i <= 7; i++) {
+    console.log(location[i]);
+    let card = `
+    <div class="col">
+        <div class="card text-white border-0 overflow-hidden">
+            <h1 class="card-title fw-bolder card-img-overlay">${loCation[i]}</h1>
+            <img src="./img/city_${i}.jpg" class="card-img" alt="...">
+        </div>
+    </div>
+    `
+    content+=card
+    cityContainer.innerHTML = content
+}
 
 
 
-let restaurantCard = []
-const cors = 'https://cors-anywhere.herokuapp.com/';
-const url = 'https://picsum.photos/v2/list'
-
- 
-
-
-axios.get(`${url}`)
-.then(res => {
-    console.log(res);
-    data = res.data
-    console.log(data[0]);
-
-    let restaurantCard = data.slice(16, 24)
-
-
-    new Vue({
-        el:'#restaurant',
-        data: {
-            restaurantCard,
-    
-        }
-    })
-
-})
-.catch(err => {
-    console.error(err); 
-})
-
-
-
+console.log(i);
