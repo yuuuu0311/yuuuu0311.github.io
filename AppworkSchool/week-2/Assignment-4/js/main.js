0ce4baf810cd4b8c5e9180d4980203cfc3dfaf0c
yuuuu0311 <a26430803@gmail.com>
2024-01-21 16:10:00 +0800

    feat: click btn to show more content

--- AppworkSchool/week-2/Assignment-4/js/main.js
+++ AppworkSchool/week-2/Assignment-4/js/main.js
@@ -2,8 +2,6 @@ const hearoFadeOutInit = () => {
     const hero = document.querySelector("#welcomHero");
     const sub = document.querySelector("#welcomSub");
 
-    console.log(hero, sub);
-
     hero.addEventListener("click", (event) => {
         hero.classList.remove("active");
         sub.classList.add("active");
@@ -20,7 +18,16 @@ const toggleNavbar = () => {
     });
 };
 
+const showContentInit = () => {
+    const btn = document.querySelector(".button-item");
+
+    btn.addEventListener("click", () => {
+        const contentGroup = document.querySelectorAll(".content-group.hidden");
+        contentGroup[0].classList.remove("hidden");
+    });
+};
 window.onload = () => {
     hearoFadeOutInit();
     toggleNavbar();
+    showContentInit();
 };
