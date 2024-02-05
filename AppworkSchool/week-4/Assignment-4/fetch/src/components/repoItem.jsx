3d1d851ac2e2b5b278c36a643f8c0b9c4626fdc3
yuuuu0311 <a26430803@gmail.com>
2024-02-05 01:09:24 +0100

    refactor: Assingment-4 with react

--- AppworkSchool/week-4/Assignment-4/fetch/src/components/repoItem.jsx
+++ AppworkSchool/week-4/Assignment-4/fetch/src/components/repoItem.jsx
@@ -1,39 +1,84 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const RepoItem = () => {
     // fetch Data
-    const fetchData = (url) => {
+    const fetchData = (url, callBack) => {
         fetch(url)
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
-                return data;
-                // callBack(data);
+                // return data;
+                callBack(data);
             });
     };
 
     // state
     const [number, setNumber] = useState(5);
     const [page, setPage] = useState(1);
-    const [repoList, setRepoList] = useState(() => {});
+    const [repoList, setRepoList] = useState([]); // 空陣列
 
-    const repoData = fetchData(
-        `https://api.github.com/orgs/facebook/repos?per_page=${number}&page=${page}`
-        // (data) => {
-        //     data.map((repo) => {
-        //         const { name, visibility, description, topics } = repo;
-        //         // console.log(name, visibility, description, topics);
-        //     });
-        // }
-    );
+    // handler
+    const handlePageChange = (num) => {
+        setPage(page + num);
+    };
 
-    console.log(repoData);
+    // render list
+    useEffect(() => {
+        const url = `https://api.github.com/orgs/facebook/repos?per_page=${number}&page=${page}`;
+
+        fetchData(url, (data) => {
+            setRepoList(data);
+        });
+    }, [number, page]); // Depend on 'number' and 'page' changes
 
     return (
-        <ul>
-            <li>repo</li>
-        </ul>
+        <section>
+            <ul>
+                {repoList.map((repo) => {
+                    const { id, name, visibility, description, topics } = repo;
+
+                    return (
+                        <li key={id}>
+                            <div>
+                                <span className="name">{name}</span>
+                                <span className="visibility">{visibility}</span>
+                            </div>
+                            <div>
+                                <span className="description">
+                                    {description}
+                                </span>
+                            </div>
+                            <div className="topics">
+                                {topics.map((topic, index) => {
+                                    return (
+                                        <span key={index} className="topic">
+                                            {topic}
+                                        </span>
+                                    );
+                                })}
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+            <div>
+                <button
+                    onClick={() => {
+                        handlePageChange(-1);
+                    }}
+                >
+                    prev page
+                </button>
+                <button
+                    onClick={() => {
+                        handlePageChange(1);
+                    }}
+                >
+                    next page
+                </button>
+            </div>
+        </section>
     );
 };
 
